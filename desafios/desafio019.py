23a87144758559a521106f2b3796767d49ba20c7
Myrla Rodrigues <myrlarodriguesarq@gmail.com>
2025-06-17 20:54:45 -0300

    Adição de novos desafios

--- desafios/desafio019.py
+++ desafios/desafio019.py
@@ -1,5 +1,8 @@
 import random
-alunos = input ("Digite o nome dos alunos que participarão do sorteio, sepadandos por vírgula: ")
-lista_alunos = [nome.strip() for nome in alunos.split(",")]
-aluno_random = random.choice(lista_alunos)
-print(f'O aluno sorteado é: {aluno_random}')
+n1 = str(input("Primeiro aluno: "))
+n2 = str(input("Segundo aluno: "))
+n3 = str(input("Terceiro aluno: "))
+n4 = str(input("Quarto aluno: "))
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print(f'O aluno escolhido foi {escolhido}')
---
+++ desafios/desafio021.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('desafios/ex021.mp3')
+pygame.mixer.music.play()
+input('Pressione Ent4er para encerrar...')
